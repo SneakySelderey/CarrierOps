6131477d141953154902dbe6aa2f124d751ad94e
A. Ershov <you@example.com>
2021-12-23 21:16:44 +0300

    Player class moved to player.py

--- main.py
+++ main.py
@@ -2,26 +2,7 @@ import pygame
 import random
 from math import sqrt
 from board import Board
-
-
-# класс, определяющий параметеры и спрайт игрока
-class Player(pygame.sprite.Sprite):
-    def __init__(self, visibility):
-        pygame.sprite.Sprite.__init__(self)
-        player_img = pygame.image.load('data/img/Player_cursor.png').convert()
-        self.image = player_img
-        self.image.set_colorkey(pygame.Color('black'))
-        self.rect = self.image.get_rect()
-        self.rect.center = [25, 25]
-        self.speedx = 0
-        self.speedy = 0
-
-        self.visibility = visibility
-
-    # обновление позиции спрайта
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
+from player import Player
 
 
 # класс, определяющий параметры и спрайт ИИ
