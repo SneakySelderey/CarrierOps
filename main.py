af8a1fb0456fc589b54840b4851a59121acf443e
A. Ershov <you@example.com>
2021-12-23 21:19:08 +0300

    AI class moved to AI.py

--- main.py
+++ main.py
@@ -3,26 +3,7 @@ import random
 from math import sqrt
 from board import Board
 from player import Player
-
-
-# класс, определяющий параметры и спрайт ИИ
-class AI(pygame.sprite.Sprite):
-    def __init__(self, board, visibility, cell_size):
-        pygame.sprite.Sprite.__init__(self)
-        player_img = pygame.image.load('data/img/AI_cursor.png').convert()
-        self.image = player_img
-        self.image.set_colorkey(pygame.Color('black'))
-        self.rect = self.image.get_rect()
-        self.rect.center = [cell_size * board.width, cell_size * board.height]
-        self.speedx = 0
-        self.speedy = 0
-
-        self.visibility = visibility
-
-    # обновление позиции спрайта
-    def update(self):
-        self.rect.x += self.speedx
-        self.rect.y += self.speedy
+from AI import AI
 
 
 # класс, определяющий спрайт и местоположение базы-острова
