e393f7b51625b36266111fabce47d1232c90ef74
Ivan <st.affeev@yandex.ru>
2021-12-26 18:02:29 +0300

    AI appears in a random place on the right side of the screen

--- AI.py
+++ AI.py
@@ -1,4 +1,5 @@
 import pygame
+from random import randint
 from Settings import BLACK, AI_IMAGE
 
 
@@ -9,7 +10,8 @@ class AI(pygame.sprite.Sprite):
         self.image = AI_IMAGE.convert()
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.center = [cell_size * board.width, cell_size * board.height]
+        self.rect.center = [cell_size * board.width,
+                            randint(0, cell_size * board.height)]
         self.speedx = 0
         self.speedy = 0
 
