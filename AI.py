67288fe4a49d4bb44255d9fcc177ae3fafbe24fd
Ivan <st.affeev@yandex.ru>
2021-12-27 10:15:02 +0300

    now AI's and player's ships adjust under the size of the cell

--- AI.py
+++ AI.py
@@ -1,13 +1,16 @@
 import pygame
 from random import randint
-from Settings import BLACK, AI_IMAGE, WIDTH, HEIGHT
+from Settings import BLACK, AI_IMAGE, WIDTH, HEIGHT, CELL_SIZE
 
 
 class AI(pygame.sprite.Sprite):
     """Класс, определяющий параметры и спрайт ИИ"""
     def __init__(self, visibility):
         super().__init__()
-        self.image = AI_IMAGE
+        image = AI_IMAGE
+        x, y = image.get_size()
+        self.image = pygame.transform.scale(image, (
+            x * x // (70 + CELL_SIZE), y * y // (70 + CELL_SIZE)))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = [WIDTH, randint(0, HEIGHT)]
--- player.py
+++ player.py
@@ -1,13 +1,16 @@
 import pygame
 from random import randint
-from Settings import BLACK, PLAYER_IMAGE, HEIGHT
+from Settings import BLACK, PLAYER_IMAGE, HEIGHT, CELL_SIZE
 
 
 class Player(pygame.sprite.Sprite):
     """Класс, определяющий параметры и спрайт игрока"""
     def __init__(self, visibility):
         super().__init__()
-        self.image = PLAYER_IMAGE
+        image = PLAYER_IMAGE
+        x, y = image.get_size()
+        self.image = pygame.transform.scale(image, (
+            x * x // (70 + CELL_SIZE), y * y // (70 + CELL_SIZE)))
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.center = [40, randint(40, HEIGHT - 40)]
