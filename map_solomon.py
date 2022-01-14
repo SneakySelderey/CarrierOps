ca163fe77636643bc4d2f8c452fe2cf6a74a8e1e
A. Ershov <sneakyselderey@yandex.ru>
2022-01-15 01:10:10 +0300

    LandCheck class returned. AI paths fixed.

--- map_solomon.py
+++ map_solomon.py
@@ -35,3 +35,26 @@ class Map(pygame.sprite.Sprite):
         del to_save['_Sprite__g'], to_save['image'], to_save['mask'], \
             to_save['parent_board']
         return to_save
+
+
+class LandCheck(pygame.sprite.Sprite):
+    def __init__(self, visibility, board, run):
+        super().__init__()
+        self.image = pygame.transform.scale(Settings.LAND_CHECK_IMG, (
+            Settings.CELL_SIZE * 0.6, Settings.CELL_SIZE * 0.6))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (0, 0)
+        self.visibility = visibility
+        self.mask = pygame.mask.from_surface(self.image)
+        self.parent_board = board
+        Settings.ALL_SPRITES_FOR_SURE.add(self)
+
+        for x in range(board.width):
+            for y in range(board.height):
+                self.rect.center = (Settings.CELL_SIZE * x + Settings.CELL_SIZE / 2,
+                                    Settings.CELL_SIZE * y + Settings.CELL_SIZE / 2)
+                if pygame.sprite.collide_mask(self, run.map):
+                    Settings.BOARD[y][x] = 'X'
+
+    def new_position(self, cell, top, left):
+        pass
